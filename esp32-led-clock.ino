2ec12ed42f28e2d2ff3506882866270df4aee298
Manish Pandya <manish.pandya@gmail.com>
2023-01-26 03:31:59 -0500

    pretty formatting

--- esp32-led-clock.ino
+++ esp32-led-clock.ino
@@ -5,24 +5,37 @@
 
 const char *ssid = "reboot";
 const char *password = "91265264109220132502751234";
-const char* ntpServer = "pool.ntp.org";
+const char *ntpServer = "pool.ntp.org";
 const long  gmtOffset_sec = -18000; // NYC
 const int   daylightOffset_sec = 3600;
 
-#define latchPin 5
-#define clockPin 18
-#define dataPin 23
-#define onboardLED 2
-#define secondsLED 3
-#define pwmFreq 5000
+#define SR_LATCH_GPIO 5
+#define SR_CLOCK_GPIO 18
+#define SR_DATA_PIN 23
+#define ONBOARD_BLUE_LED_GPIO 2
+#define SECONDS_LED_GPIO 3
+#define PWM_FREQUENCY 5000
 #define pwmResolution 8
-#define secondsLEDPWMChannel 0
-#define LDRPin 34
+#define SECONDS_LED_PWM_CHANNEL 0
+#define LDR_GPIO 34
 
+// Seven segment display bit mask (active low)
+#define DISP_BLANK 10
+#define DISP_A 11
+#define DISP_C 12
+#define DISP_E 13
+#define DISP_F 14
+#define DISP_P 15
 
+byte table[16] {
+  B00000011, B10011111, B00100101, B00001101,
+  B10011001, B01001001, B01000001, B00011111,
+  B00000001, B00001001, B11111111, B00010001,
+  B01100011, B01100001, B01110001, B00110001
+};
+// -------------------------------------------
 
-byte table[10] {B00000011, B10011111, B00100101, B00001101, B10011001, B01001001, B01000001, B00011111, B00000001, B00001001};
-int flashcount = 1;
+int totalFlashcount = 1;
 
 struct tm timeinfo;
 
@@ -36,9 +49,9 @@ void printLocalTime()
 }
 
 void Display() {
-  digitalWrite(latchPin, LOW);
+  digitalWrite(SR_LATCH_GPIO, LOW);
   int displayDigit = 0;
-  switch(flashcount) {
+  switch(totalFlashcount) {
     case 1:
       displayDigit = timeinfo.tm_hour / 10;
       break;
@@ -53,55 +66,73 @@ void Display() {
       break;
     case 5:
       screenOff();
-      flashcount = 0;
+      totalFlashcount = 0;
       if(!getLocalTime(&timeinfo)){
         Serial.println("Failed to obtain time");
       }
       break;
     default:
-      flashcount = 0;
+      totalFlashcount = 0;
       break;
   }
-  flashcount++;
+  totalFlashcount++;
   
-  shiftOut(dataPin, clockPin, LSBFIRST, table[displayDigit]); 
-  digitalWrite(latchPin, HIGH); 
+  shiftOut(SR_DATA_PIN, SR_CLOCK_GPIO, LSBFIRST, table[displayDigit]); 
+  digitalWrite(SR_LATCH_GPIO, HIGH); 
 }
 
 void screenOff() { 
-  digitalWrite(latchPin, LOW); 
-  shiftOut(dataPin, clockPin, LSBFIRST, B11111111); 
-  digitalWrite(latchPin, HIGH); 
+  digitalWrite(SR_LATCH_GPIO, LOW); 
+  shiftOut(SR_DATA_PIN, SR_CLOCK_GPIO, LSBFIRST, table[DISP_BLANK]); 
+  digitalWrite(SR_LATCH_GPIO, HIGH); 
+}
+
+int brightness = 255;
+
+void setBrightness() {
+  if (analogRead(LDR_GPIO) < 1200) {
+    brightness = 10;
+  } else {
+    brightness = 255;
+  }
+}
+
+void readSettings() {
+    int address = 0;
+    //Serial.print
+}
+
+void writeSettings() {
+  
 }
 
 void setup() {
     Serial.begin(115200);
 
-    pinMode(clockPin, OUTPUT);
-    //pinMode(LDRPin, INPUT);
-    pinMode(latchPin, OUTPUT);
-    pinMode(dataPin, OUTPUT);
-    pinMode(onboardLED, OUTPUT);
-    ledcSetup(secondsLEDPWMChannel, pwmFreq, pwmResolution);
-    ledcAttachPin(secondsLED, secondsLEDPWMChannel);
+    pinMode(SR_CLOCK_GPIO, OUTPUT);
+    pinMode(SR_LATCH_GPIO, OUTPUT);
+    pinMode(SR_DATA_PIN, OUTPUT);
+    pinMode(ONBOARD_BLUE_LED_GPIO, OUTPUT);
+    ledcSetup(SECONDS_LED_PWM_CHANNEL, PWM_FREQUENCY, pwmResolution);
+    ledcAttachPin(SECONDS_LED_GPIO, SECONDS_LED_PWM_CHANNEL);
 
     //---Connect ESP32 to Wifi Network---
     WiFi.mode(WIFI_STA);
     WiFi.begin(ssid, password);
     Serial.println("");
     // Wait for connection
-    bool onboardLEDStatus = HIGH;
+    bool ONBOARD_BLUE_LED_GPIOStatus = HIGH;
     int counter = 0;
     while (WiFi.status() != WL_CONNECTED && counter < 60) {
         delay(500);
-        digitalWrite(onboardLED, onboardLEDStatus);
-        onboardLEDStatus = !onboardLEDStatus;
+        digitalWrite(ONBOARD_BLUE_LED_GPIO, ONBOARD_BLUE_LED_GPIOStatus);
+        ONBOARD_BLUE_LED_GPIOStatus = !ONBOARD_BLUE_LED_GPIOStatus;
         counter++;
     }
     if (WiFi.status() != WL_CONNECTED) {
-        digitalWrite(onboardLED, HIGH);
+        digitalWrite(ONBOARD_BLUE_LED_GPIO, HIGH);
     } else {
-      digitalWrite(onboardLED, LOW);
+      digitalWrite(ONBOARD_BLUE_LED_GPIO, LOW);
       //Display WiFi info to Serial Monitor on successful connection
       Serial.println("");
       Serial.print("Connected to ");
@@ -114,28 +145,15 @@ void setup() {
     printLocalTime();
 }
 
-int brightness = 255;
-
-void setBrightness() {
-  if (analogRead(LDRPin) < 1200) {
-    brightness = 10;
-  } else {
-    brightness = 255;
-  }
-}
-
 void loop() {
-    //digitalWrite(secondsLED, LOW);
+    //digitalWrite(SECONDS_LED_GPIO, LOW);
     setBrightness();
-    ledcWrite(secondsLEDPWMChannel, 255 - brightness);
-    Serial.println(analogRead(LDRPin));
+    ledcWrite(SECONDS_LED_PWM_CHANNEL, 255 - brightness);
     Display();
     delay(800);
     screenOff();
     delay(200);
-    //digitalWrite(secondsLED, HIGH);
-    ledcWrite(secondsLEDPWMChannel, 256);
-    Serial.println(analogRead(LDRPin));
+    //digitalWrite(SECONDS_LED_GPIO, HIGH);
+    ledcWrite(SECONDS_LED_PWM_CHANNEL, 256);
     delay(1000);
-    Serial.println(brightness);
 }
